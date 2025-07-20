c13a4b1f1ccb44864a38beaffff2fed87b8d7f7c
SyqJzcX <sqzx0524@163.com>
2025-07-21 03:06:11 +0800

    Update.

--- img2latex_local.py
+++ img2latex_local.py
@@ -1,5 +1,4 @@
-import openai
-from qwen_api import LlmApi
+from qwen_local import LlmLocal
 from typing import List
 
 
@@ -9,10 +8,7 @@ def img2latex_local(model: str, image_path: str) -> str:
         model=model
     )
 
-    prompt_list = [
-        {
-            "type": "text",
-            "text": """
+    prompt = """
                 你是一个专业的LaTeX公式识别专家。
                 请将图片中的数学公式准确转换为LaTeX代码。
                 要求：
@@ -20,14 +16,8 @@ def img2latex_local(model: str, image_path: str) -> str:
                 2. 公式两边不需要加入任何额外的数学环境标记(如 \\[ \\] 或 \\( \\) 等)
                 3. 如果公式末尾包含类似 ( 2 - 1 ) 这样的公式编号，请将这部分去掉
                 """
-        },
-        {
-            "type": "image_url",
-            "image_url": f"{image_path}"
-        }
-    ]
 
-    answer = qwen_local.get_response(prompt_list)
+    answer = qwen_local.get_response(prompt, image_path)
 
     return answer
 
@@ -35,7 +25,7 @@ def img2latex_local(model: str, image_path: str) -> str:
 if __name__ == "__main__":
     latex = img2latex_local(
         model="qwen2.5vl:7b",
-        image_path='./formula.png'
+        image_path='D:\\桌面文件\\Ykz_llm_local\\formula.png'
     )
 
     print(latex)
